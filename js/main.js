9efd246bb0867bf80bb3ee0315a00e4b5dc6efcd
Yurii Moroziuk <Yuriy.Morozyuk.95@gmail.com>
2019-11-10 15:56:29 +0200

    fix background image and js for slider

--- js/main.js
+++ js/main.js
@@ -74,9 +74,5 @@ function showProgramSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideProgramIndex-1].style.display = "block";  
-  dots[slideProgramIndex-1].className += " active";
+  slides[slideProgramIndex-1].style.display = "block";
 }
