a929df65625d3c0fb935110e6e894150bb4c4b61
Yurii Moroziuk <Yuriy.Morozyuk.95@gmail.com>
2019-11-15 12:00:34 +0200

    fix radio

--- js/main.js
+++ js/main.js
@@ -1,34 +1,35 @@
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
 // Option 2 -
-$(".navbar a").on("click", function(e) {
-    if(this.hash != "") {
-        e.preventDefault();
+$(".navbar a").on("click", function (e) {
+  if (this.hash != "") {
+      e.preventDefault();
 
-        const hash = this.hash;
+      const hash = this.hash;
 
-        $("html, body").animate({
-            scrollTop: $(hash).offset().top
-        }, 800);
-    }
+      $("html, body").animate({
+          scrollTop: $(hash).offset().top
+      }, 800);
+  }
 });
 
 function myMap() {
 
-    let map = new google.maps.Map(document.getElementById('googleMap'), {
-        center: new google.maps.LatLng(59.405650, 24.734890),
-        zoom: 18
-      });
-    let radioRelax = new google.maps.LatLng(59.405650, 24.734890);
+  var map = new google.maps.Map(document.getElementById('googleMap'), {
+      center: new google.maps.LatLng(59.405650, 24.734890),
+      zoom: 18
+  });
+  var radioRelax = new google.maps.LatLng(59.405650, 24.734890);
 
-    let homeMarker = new google.maps.Marker({
-        map: map,
-        position: radioRelax,
-        animation: google.maps.Animation.DROP,
-        title: 'Radio Relax'
-      });
+  var homeMarker = new google.maps.Marker({
+      map: map,
+      position: radioRelax,
+      animation: google.maps.Animation.DROP,
+      title: 'Radio Relax'
+  });
 }
-
-var slideIndex = 1;
-showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -42,183 +43,119 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+      slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active-dot", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active-dot";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active-dot";
 }
 
-// var slideProgramIndex = 1;
-// showProgramSlides(slideProgramIndex);
-
-// function plusProgramSlides(n) {
-//   showProgramSlides(slideProgramIndex += n);
-// }
-
-// function showProgramSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("programSlides");
-//   if (n > slides.length) {slideProgramIndex = 1}    
-//   if (n < 1) {slideProgramIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";  
-//   }
-//   slides[slideProgramIndex-1].style.display = "block";
-// }
-
-jQuery(window).load(function() {
- 
-  /*
-      Stop carousel
-  */
-  $('.carousel').carousel('pause');
-
-});
-
-/* 
-Player */
-
-$(
-  function(){
-  var aud = $('audio')[0];
-  $('.play-pause').on('click', function(){
+function playOrPause() {
   if (aud.paused) {
-    aud.play();
-    $('.play-pause').removeClass('fa-play');
-    $('.play-pause').addClass('fa-pause');
+      aud.play();
+      $('.play-pause').removeClass('fa-play');
+      $('.play-pause').addClass('fa-pause');
   }
   else {
-    aud.pause();
-    $('.play-pause').removeClass('fa-pause');
-    $('.play-pause').addClass('fa-play');
+      aud.pause();
+      $('.play-pause').removeClass('fa-pause');
+      $('.play-pause').addClass('fa-play');
   }
-})
+}
 
-  $('.sound-up-down').on('click', function(){
-    var aud = $('audio')[0];
-    if (aud.muted) {
+function setupPlayer() {
+}
+
+function soundUpDown() {
+  if (aud.muted) {
       aud.muted = false;
       $('.sound-up-down').removeClass('fa-volume-down');
       $('.sound-up-down').addClass('fa-volume-up');
-    }
-    else {
+  }
+  else {
       aud.muted = true;
       $('.sound-up-down').removeClass('fa-volume-up');
       $('.sound-up-down').addClass('fa-volume-down');
-    }
-})
+  }
+}
 
-$('#RadioRelaxStream').on('click', function(){
-  var aud = $('audio')[0];
-  aud.src='http://37.0.31.66:8000/relax';
-  if (aud.paused)
-  {
-    aud.play();
-    $('.play-pause').removeClass('fa-play');
-    $('.play-pause').addClass('fa-pause');
-  }
-  else{
-    aud.pause();
-    $('.play-pause').removeClass('fa-pause');
-    $('.play-pause').addClass('fa-play');
-  }
-  $
+
+$('#RadioRelaxStream').on('click', function () {
+  aud.src = 'http://37.0.31.66:8000/relax';
   $('#currentRadio').text("Relax Radio");
+  playOrPause();
 })
 
-
-$('#RadioRelaxInternationalStream').on('click', function(){
-  var aud = $('audio')[0];
-  aud.src='http://37.0.31.66:8000/international';
-  if (aud.paused)
-  {
-    aud.play();
-    $('.play-pause').removeClass('fa-play');
-    $('.play-pause').addClass('fa-pause');
-  }
-  else{
-    aud.pause();
-    $('.play-pause').removeClass('fa-pause');
-    $('.play-pause').addClass('fa-play');
-  }
-  $('.play-pause').removeClass('fa-pause');
-  $('.play-pause').addClass('fa-play');
+$('#RadioRelaxInternationalStream').on('click', function () {
+  aud.src = 'http://37.0.31.66:8000/international';
   $('#currentRadio').text("Relax International");
+  playOrPause();
 })
 
-
-$('#RadioRelaxIndoorStream').on('click', function(){
-  var aud = $('audio')[0];
-  aud.src='http://37.0.31.66:8000/instrumental';
-  if (aud.paused)
-  {
-    aud.play();
-    $('.play-pause').removeClass('fa-play');
-    $('.play-pause').addClass('fa-pause');
-  }
-  else{
-    aud.pause();
-    $('.play-pause').removeClass('fa-pause');
-    $('.play-pause').addClass('fa-play');
-  }
-  $('.play-pause').removeClass('fa-pause');
-  $('.play-pause').addClass('fa-play');
+$('#RadioRelaxIndoorStream').on('click', function () {
+  aud.src = 'http://37.0.31.66:8000/instrumental';
   $('#currentRadio').text("Relax Indoor");
+  playOrPause();
 })
 
-
-$('#RadioRelaxCafeStream').on('click', function(){
-  var aud = $('audio')[0];
-  aud.src='http://37.0.31.66:8000/cafe';
-  if (aud.paused)
-  {
-    aud.play();
-    $('.play-pause').removeClass('fa-play');
-    $('.play-pause').addClass('fa-pause');
-  }
-  else{
-    aud.pause();
-    $('.play-pause').removeClass('fa-pause');
-    $('.play-pause').addClass('fa-play');
-  }
-  $('.play-pause').removeClass('fa-pause');
-  $('.play-pause').addClass('fa-play');
+$('#RadioRelaxCafeStream').on('click', function () {
+  aud.src = 'http://37.0.31.66:8000/cafe';
   $('#currentRadio').text("Relax Cafe");
+  playOrPause();
 })
 
-$('#RadioRelaxVeneStream').on('click', function(){
-  var aud = $('audio')[0];
-  aud.src='http://37.0.31.66:8000/vene';
-  if (aud.paused)
-  {
-    aud.play();
-    $('.play-pause').removeClass('fa-play');
-    $('.play-pause').addClass('fa-pause');
-  }
-  else{
-    aud.pause();
-    $('.play-pause').removeClass('fa-pause');
-    $('.play-pause').addClass('fa-play');
-  }
-  $('.play-pause').removeClass('fa-pause');
-  $('.play-pause').addClass('fa-play');
-  $('#currentRadio').text("Relax Vene");
+$('#RadioRelaxVeneStream').on('click', function () {
+  aud.src = 'http://37.0.31.66:8000/vene';
+  $('#currentRadio').text("Русский Relax");
+  playOrPause();
 })
 
+function SetVolume(val) {
+  var player = document.getElementById('audioplayer');
+  player.volume = val / 100;
+}
 
+var aud = $('audio')[0];
+var slideIndex = 1;
+showSlides(slideIndex);
+$('.play-pause').on('click', playOrPause);
+$('.sound-up-down').on('click', soundUpDown);
+myMap();
+$('.carousel').carousel('pause');
 
-})
+//function getSongInfo() {
 
-function SetVolume(val)
-    {
-        var player = document.getElementById('audioplayer');
-        // console.log('Before: ' + player.volume);
-        player.volume = val / 100;
-        // console.log('After: ' + player.volume);
-    }
+//    var stream = $('#artist').attr("data-id");
+
+//    var streamUrl = "/api/SongNameApi/" + stream;
+
+//    $.ajax({
+//        type: "GET",
+//        url: streamUrl,
+//        contentType: "application/json; charset=utf-8",
+//        dataType: "json",
+//        success: function (data) {
+
+//            $('#artist').text(data.artist);
+//            $('#title').text(data.title);
+
+//            console.log(data);
+//        }, //End of AJAX Success function  
+
+//        failure: function (data) {
+//            console.log(data.responseText); //alert(data.responseText);
+//        }, //End of AJAX failure function  
+//        error: function (data) {
+//            console.log(data.responseText); //alert(data.responseText);
+//        } //End of AJAX error function  
+
+//    });
+//};
+
+//getSongInfo();
+//setInterval(getSongInfo, 10000);
